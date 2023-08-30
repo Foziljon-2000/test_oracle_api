0bd4854d43fba3fdd734611fa1c45872e0453693
Fozil_Jon <120250186+Foziljon-2000@users.noreply.github.com>
2023-08-30 17:30:49 +0500

    +feker

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,11 +5,17 @@ import (
 
 	"github.com/Foziljon-2000/test_oracle_api/internal/models"
 	"github.com/gin-gonic/gin"
+	"github.com/go-faker/faker/v4"
 )
 
 func JsonGan(c *gin.Context) {
+	var txt []models.User
 
-	txt := &models.User{Name:"Youssef", ID: 1}
+	for i:=1;i<=50;i++{
+		name := faker.Name()
+		
+		txt = append(txt, models.User{Name: name, ID: i})
+	}
 
 	c.JSON(http.StatusOK, txt)
 }
